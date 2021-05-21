901822857bc558a28c3c19ed5ea4ede9380273d9
Khamit Amantaev <khamitamantaev@gmail.com>
2021-05-21 23:00:54 +0500

    signupRouter is working fine on Redux.

---
+++ bootstrap_front-end/src/core/signup/signup.router.js
@@ -0,0 +1,7 @@
+import { setActivePath, SetActivePath} from '../../lib/common/navigation';
+
+import { SIGNUP_ROUTE_PATH } from './signup.constant';
+
+export async function SignupRouter(ctx) {
+  ctx.store.dispatch(setActivePath(SIGNUP_ROUTE_PATH))
+  }
--- bootstrap_front-end/src/main/router/index.js
+++ bootstrap_front-end/src/main/router/index.js
@@ -1,5 +1,8 @@
 import { routesInit } from './router.core';
+import {SignupRouter, SIGNUP_ROUTE_PATH } from '../../core/signup';
+export const routes = {
+    [SIGNUP_ROUTE_PATH]:SignupRouter 
 
-export const routes = {};
+};
 
 export const Router = routesInit(routes);

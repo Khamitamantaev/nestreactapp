3a74a0c2d7af958796cfeacee9d376f6597f40b6
Khamit Amantaev <khamitamantaev@gmail.com>
2021-05-21 22:26:42 +0500

    Hello Khamit is working nice!

---
+++ bootstrap_front-end/src/core/signup/signup.constant.js
@@ -0,0 +1,2 @@
+export const SIGNUP_ROUTE_PATH = '/signup'
+// export const SIGNUP_ROUTE_PATH_DINAMIC = () => '/signup'
---
+++ bootstrap_front-end/src/core/signup/signup.page.js
@@ -0,0 +1,5 @@
+export function SignupPage() {
+  return (
+    <h1>Hello Khamit!</h1>
+  )
+}

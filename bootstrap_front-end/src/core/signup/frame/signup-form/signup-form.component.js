a2f09726baaefbd280e31647c0c7beb0f291e58f
Khamit Amantaev <khamitamantaev@gmail.com>
2021-05-22 17:17:00 +0500

    FieldPrimary вместо инпутов результат

--- bootstrap_front-end/src/core/signup/frame/signup-form/signup-form.component.js
+++ bootstrap_front-end/src/core/signup/frame/signup-form/signup-form.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FieldPrimary } from '../../../../lib/elements/field/field.primary';
 
 export function SignupFormComponent(props) {
   const {
@@ -14,33 +15,32 @@ export function SignupFormComponent(props) {
   } = props;
 
   const isFieldError = (name) => {
-    return (
-      errors[name] &&
-      touched[name] &&
-      errors[name]
-    );
+    return errors[name] && touched[name] && errors[name];
   };
-
-  
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
+        <FieldPrimary
+          titleTid="SIGNUP.SIGNUP_FORM.FIELD.LOGIN.TITLE"
+          placeholderTid="SIGNUP.SIGNUP_FORM.FIELD.LOGIN.PLACEHOLDER"
           name={fieldLogin}
           onChange={handleChange}
           onBlur={handleBlur}
           value={values[fieldLogin]}
+          error={isFieldError(fieldLogin)}
         />
-        {isFieldError(fieldLogin)}
-        <input
-          type="password"
+        
+        <FieldPrimary
+          titleTid="SIGNUP.SIGNUP_FORM.FIELD.PASSWORD.TITLE"
+          placeholderTid="SIGNUP.SIGNUP_FORM.FIELD.PASSWORD.PLACEHOLDER"
           name={fieldPassword}
           onChange={handleChange}
           onBlur={handleBlur}
           value={values[fieldPassword]}
+          error={isFieldError(fieldPassword)}
         />
-        {isFieldError(fieldPassword)}
+        
         <button type="submit" disabled={isSubmitting}>
           Submit
         </button>
